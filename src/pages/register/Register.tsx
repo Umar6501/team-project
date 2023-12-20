850850a4a4565c1f4b78602ab859223c4620fb60
Asadov Elbotir <elbotir2008@gmail.com>
2023-12-20 21:48:40 +0500

    router qoshuldi

---
+++ src/pages/register/Register.tsx
@@ -0,0 +1,10 @@
+import "./register.scss";
+const Register = () => {
+  return (
+    <div className="register-container">
+      <div className="register-block"></div>
+    </div>
+  );
+};
+
+export default Register;
