92892712ead89bba55d7fda016301f62f098061b
Asadov Elbotir <elbotir2008@gmail.com>
2023-12-20 22:17:57 +0500

    login bilan registerni qo'shdim

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,33 @@
-import React from "react";
-
+import { Link } from "react-router-dom";
+import "./login.scss";
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login-container">
+      <div className="login-block">
+        <h1>CRUD OPERATIONS</h1>
+        <h5>SIGN IN</h5>
+        <p>Enter your credentials to access your account</p>
+        <label htmlFor="email">Email</label>
+        <div className="input1">
+          <input type="email" id="email" placeholder="Enter your email" />
+        </div>
+        <label htmlFor="password">Password</label>
+        <div className="input2">
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter your password"
+          />
+        </div>
+        <Link to="/home">
+          <button>SIGN IN</button>
+        </Link>
+        <p>
+          Forgot your password? <span>Reset Password</span>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,8 +1,31 @@
+import { Link } from "react-router-dom";
 import "./register.scss";
 const Register = () => {
   return (
     <div className="register-container">
-      <div className="register-block"></div>
+      <div className="register-block">
+        <h1>CRUD OPERATIONS</h1>
+        <h5>SIGN UP</h5>
+        <p>Enter your credentials to access your account</p>
+        <label htmlFor="email">Email</label>
+        <div className="input1">
+          <input type="email" id="email" placeholder="Enter your email" />
+        </div>
+        <label htmlFor="password">Password</label>
+        <div className="input2">
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter your password"
+          />
+        </div>
+        <Link to="/login">
+          <button>SIGN UP</button>
+        </Link>
+        <p>
+          Forgot your password? <span>Reset Password</span>
+        </p>
+      </div>
     </div>
   );
 };
